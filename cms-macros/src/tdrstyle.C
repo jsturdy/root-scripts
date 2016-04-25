107733fd52d7e32a370fa87d2235b56d0002381d
Jared Sturdy <sturdy@cern.ch>
2016-04-25 10:01:34 +0200

    cleaned up tdrstyle

--- cms-macros/src/tdrstyle.C
+++ cms-macros/src/tdrstyle.C
@@ -16,7 +16,7 @@ void fixOverlay() {
 void setTDRStyle() {
   TStyle *tdrStyle = new TStyle("tdrStyle","Style for P-TDR");
 
-// For the canvas:
+  // For the canvas:
   tdrStyle->SetCanvasBorderMode(0);
   tdrStyle->SetCanvasColor(kWhite);
   tdrStyle->SetCanvasDefH(600); //Height of canvas
@@ -24,7 +24,7 @@ void setTDRStyle() {
   tdrStyle->SetCanvasDefX(0);   //POsition on screen
   tdrStyle->SetCanvasDefY(0);
 
-// For the Pad:
+  // For the Pad:
   tdrStyle->SetPadBorderMode(0);
   // tdrStyle->SetPadBorderSize(Width_t size = 1);
   tdrStyle->SetPadColor(kWhite);
@@ -34,7 +34,7 @@ void setTDRStyle() {
   tdrStyle->SetGridStyle(3);
   tdrStyle->SetGridWidth(1);
 
-// For the frame:
+  // For the frame:
   tdrStyle->SetFrameBorderMode(0);
   tdrStyle->SetFrameBorderSize(1);
   tdrStyle->SetFrameFillColor(0);
@@ -43,7 +43,7 @@ void setTDRStyle() {
   tdrStyle->SetFrameLineStyle(1);
   tdrStyle->SetFrameLineWidth(1);
   
-// For the histo:
+  // For the histo:
   // tdrStyle->SetHistFillColor(1);
   // tdrStyle->SetHistFillStyle(0);
   tdrStyle->SetHistLineColor(1);
@@ -58,19 +58,19 @@ void setTDRStyle() {
   
   tdrStyle->SetMarkerStyle(20);
   
-//For the fit/function:
+  // For the fit/function:
   tdrStyle->SetOptFit(1);
   tdrStyle->SetFitFormat("5.4g");
   tdrStyle->SetFuncColor(2);
   tdrStyle->SetFuncStyle(1);
   tdrStyle->SetFuncWidth(1);
 
-//For the date:
+  // For the date:
   tdrStyle->SetOptDate(0);
   // tdrStyle->SetDateX(Float_t x = 0.01);
   // tdrStyle->SetDateY(Float_t y = 0.01);
 
-// For the statistics box:
+  // For the statistics box:
   tdrStyle->SetOptFile(0);
   tdrStyle->SetOptStat(0); // To display the mean and RMS:   SetOptStat("mr");
   tdrStyle->SetStatColor(kWhite);
@@ -85,13 +85,13 @@ void setTDRStyle() {
   // tdrStyle->SetStatX(Float_t x = 0);
   // tdrStyle->SetStatY(Float_t y = 0);
 
-// Margins:
+  // Margins:
   tdrStyle->SetPadTopMargin(0.05);
   tdrStyle->SetPadBottomMargin(0.13);
   tdrStyle->SetPadLeftMargin(0.16);
   tdrStyle->SetPadRightMargin(0.02);
 
-// For the Global title:
+  // For the Global title:
 
   tdrStyle->SetOptTitle(0);
   tdrStyle->SetTitleFont(42);
@@ -106,7 +106,7 @@ void setTDRStyle() {
   // tdrStyle->SetTitleStyle(Style_t style = 1001);
   // tdrStyle->SetTitleBorderSize(2);
 
-// For the axis titles:
+  // For the axis titles:
 
   tdrStyle->SetTitleColor(1, "XYZ");
   tdrStyle->SetTitleFont(42, "XYZ");
@@ -117,14 +117,14 @@ void setTDRStyle() {
   tdrStyle->SetTitleYOffset(1.25);
   // tdrStyle->SetTitleOffset(1.1, "Y"); // Another way to set the Offset
 
-// For the axis labels:
+  // For the axis labels:
 
   tdrStyle->SetLabelColor(1, "XYZ");
   tdrStyle->SetLabelFont(42, "XYZ");
   tdrStyle->SetLabelOffset(0.007, "XYZ");
   tdrStyle->SetLabelSize(0.05, "XYZ");
 
-// For the axis:
+  // For the axis:
 
   tdrStyle->SetAxisColor(1, "XYZ");
   tdrStyle->SetStripDecimals(kTRUE);
@@ -133,12 +133,12 @@ void setTDRStyle() {
   tdrStyle->SetPadTickX(1);  // To get tick marks on the opposite side of the frame
   tdrStyle->SetPadTickY(1);
 
-// Change for log plots:
+  // Change for log plots:
   tdrStyle->SetOptLogx(0);
   tdrStyle->SetOptLogy(0);
   tdrStyle->SetOptLogz(0);
 
-// Postscript options:
+  // Postscript options:
   tdrStyle->SetPaperSize(20.,20.);
   // tdrStyle->SetLineScalePS(Float_t scale = 3);
   // tdrStyle->SetLineStyleString(Int_t i, const char* text);
@@ -157,4 +157,5 @@ void setTDRStyle() {
 
   tdrStyle->cd();
 
+  std::cout << "CMS TDR Style initialized" << std::endl;
 }
